61dfb8b4c9c38df734d33f6c0828893ecc65d4d5
donna <donnacamos88@gmail.com>
2019-09-30 17:20:55 -0400

    added view files and bcrypt

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
     has_many :restaurants
-    has_many :reviews through: :restaurants 
+    has_many :reviews, through: :restaurants 
     has_secure_password 
     validates :username, presence: true, uniqueness: true 
     validates :email, presence: true, uniqueness: true 
