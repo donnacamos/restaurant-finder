fb6ea81206845d42b308d2db6531098cfa6c1991
donna <donnacamos88@gmail.com>
2019-09-30 17:09:05 -0400

    updated users_controller and created sessions controller

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,40 @@
+class SessionsController < ApplicationController
+     
+
+    def index 
+
+    end 
+
+    def new 
+        
+    end 
+
+    def create 
+       @user = User.find_by(username: params[:user][:username])
+       if @user && @user.authenticate(params[:user][:password]) 
+        session[:user_id] = @user.id
+        redirect_to user_path(@user)
+       else 
+        flash[:message] = "Sorry, please try again"
+        redirect_to login_path
+       end 
+    end 
+
+   
+
+
+
+    def home 
+    end 
+
+    def destroy 
+      session.clear 
+      redirect_to root_path 
+    end 
+
+    # private 
+
+    # def auth
+    #     request.env['omniauth.auth'] 
+    # end 
+end 
