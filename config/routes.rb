59795054c793f3f186de65f083b307ff7b487032
donna <donnacamos88@gmail.com>
2019-09-30 17:10:58 -0400

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
+
+  root 'sessions#home'
+   
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  delete '/logout' => 'sessions#destroy'
+
+  get '/signup' => 'users#new'
+  post '/signup' => 'users#create'
+
+  
   resources :companies
   resources :reviews
   resources :restaurants
