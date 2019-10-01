2d6819c2ae49ec98010137df4ee1019bff125f2a
donna <donnacamos88@gmail.com>
2019-10-01 11:10:52 -0400

    modified models

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,4 +1,6 @@
 class Restaurant < ApplicationRecord
+    belongs_to :users
+    belongs_to :companies 
     has_many :reviews
     has_many :users, through: :reviews 
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
-    has_many :restaurants
-    has_many :reviews, through: :restaurants 
+    has_many :reviews
+    has_many :reviewed_restaurants, through: :reviews, source: :restaurant 
+    has_many :restaurants  
     has_secure_password 
     validates :username, presence: true, uniqueness: true 
     validates :email, presence: true, uniqueness: true 
