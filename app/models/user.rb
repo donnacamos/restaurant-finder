db3d955846d0d5281d345beb96ddc807fa67943e
donna <donnacamos88@gmail.com>
2019-10-01 15:32:58 -0400

    combining username and email validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,12 +3,12 @@ class User < ApplicationRecord
     has_many :reviewed_restaurants, through: :reviews, source: :restaurant 
     has_many :restaurants  
     has_secure_password 
-    validates :username, presence: true, uniqueness: true 
-    validates :email, presence: true, uniqueness: true 
+    validates :username, :email, presence: true, uniqueness: true 
+    
 
-    def self.create_by_google_omniauth(auth)
-        self.find_or_create_by(username: auth[:info][:email]) do |u|
-          u.password = SecureRandom.hex
-        end
-      end
+    # def self.create_by_google_omniauth(auth)
+    #     self.find_or_create_by(username: auth[:info][:email]) do |u|
+    #       u.password = SecureRandom.hex
+    #     end
+    #   end
 end
