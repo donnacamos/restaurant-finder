89c40be98dfcd4bac0957862692c5a4a8f322a6c
donna <donnacamos88@gmail.com>
2019-10-01 13:57:13 -0400

    modified sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,57 +1,27 @@
 class SessionsController < ApplicationController
      
 
-    def index 
-
-    end 
-
-    def new 
-        @user = User.new 
-    end 
-
-    def create
-
-        if params[:provider] == 'google_oauth2'
-          @user = User.create_by_google_omniauth(auth)
-          session[:user_id] = @user.id
-          redirect_to user_path(@user)
-    
-        
-        else
-    
-          #Try to find the user in the system
-          @user = User.find_by(username: params[:user][:username])
-          #did we find someone & did they put in the right password?
-          #if @user && @user.authenticate(params[:user][:password])
-    
-          if @user && @user.authenticate(password: params[:user][:password])
-            session[:user_id] = @user.id
-            redirect_to user_path(@user)
-          else
-            flash[:error] = "Sorry, login info was incorrect. Please try again."
-            redirect_to login_path
-          end
-        end
-      end
-   
-     def home 
+    def home
     end 
 
     def destroy 
-      session.clear 
+      session.delete(:user_id)  
       redirect_to '/'
     end 
 
-    def omniauth
-        @user = User.create_by_google_omniauth(auth)
-    
-        session[:user_id] = @user.id
-        redirect_to user_path(@user)
-      end
-    
-      private
-    
-        def auth
-          request.env['omniauth.auth']
-        end
+    def new  
+    end 
+
+    def create
+      @user = User.find_by(username: params[:user][:username])
+
+      if @user && @user.authenticate(password: params[:user][:password])
+        session[:user_id] = @user.id 
+        redirect_to user_path(@user) 
+      else 
+        flash[:error] = "Sorry, please try again." 
+        redirect_to login_path 
+      end 
+    end
+   
     end
