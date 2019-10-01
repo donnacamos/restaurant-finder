6a459b7f1d4bd309d4aefe7697ab22a9b7457098
donna <donnacamos88@gmail.com>
2019-10-01 11:12:32 -0400

    added post signup route

--- config/routes.rb
+++ config/routes.rb
@@ -3,18 +3,20 @@ Rails.application.routes.draw do
   get '/' => 'sessions#home' 
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
-  get '/signup' => 'users#new'
-  post '/signup' => 'users#create'
+  get '/signup' => 'users#new' 
+  post '/signup' => 'users#create' 
   delete '/logout' => 'sessions#destroy'
 
   get '/auth/:provider/callback' => 'sessions#create'
-
-  resources :restaurants do 
-    resources :companies, only: [:new, :index]
-  end  
+ 
   resources :reviews
   resources :restaurants do 
     resources :reviews, only: [:new, :index] 
+  end 
+  resources :companies 
+  resources :restaurants 
+  resource :restaurants do
+    resources :companies, only: [:new, :index]
   end 
   resources :users, only: [:show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
