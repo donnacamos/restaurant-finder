b8bf4bc6163a19c0181d5bbaef72503ac428e00f
donna <donnacamos88@gmail.com>
2019-09-30 22:02:41 -0400

    added google omniauth

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,24 +9,32 @@ class SessionsController < ApplicationController
         
     end 
 
-    def create 
-        @user = User.find_by(username: params[:user][:username])
-        #did we find someone & did they put in the right password?
-        #if @user && @user.authenticate(params[:user][:password])
-  
-        if @user && @user.authenticate(password: params[:user][:password])
+    def create
+
+        if params[:provider] == 'google_oauth2'
+          @user = User.create_by_google_omniauth(auth)
           session[:user_id] = @user.id
           redirect_to user_path(@user)
+    
+        
         else
-          flash[:error] = "Sorry, login info was incorrect. Please try again."
-          redirect_to login_path
+    
+          #Try to find the user in the system
+          @user = User.find_by(username: params[:user][:username])
+          #did we find someone & did they put in the right password?
+          #if @user && @user.authenticate(params[:user][:password])
+    
+          if @user && @user.authenticate(password: params[:user][:password])
+            session[:user_id] = @user.id
+            redirect_to user_path(@user)
+          else
+            flash[:error] = "Sorry, login info was incorrect. Please try again."
+            redirect_to login_path
+          end
         end
       end
    
-
-
-
-    def home 
+     def home 
     end 
 
     def destroy 
@@ -34,9 +42,16 @@ class SessionsController < ApplicationController
       redirect_to root_path 
     end 
 
-    # private 
-
-    # def auth
-    #     request.env['omniauth.auth'] 
-    # end 
-end 
+    def omniauth
+        @user = User.create_by_google_omniauth(auth)
+    
+        session[:user_id] = @user.id
+        redirect_to user_path(@user)
+      end
+    
+      private
+    
+        def auth
+          request.env['omniauth.auth']
+        end
+    end
