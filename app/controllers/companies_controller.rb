c35133afacc3cc2035364d4cb14820c413818343
donna <donnacamos88@gmail.com>
2019-10-02 22:50:43 -0400

    modified companies and restaurants controllers

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,5 +1,7 @@
 class CompaniesController < ApplicationController
-    def new 
-        @company = Company.new 
+    before_action :redirect_if_not_logged_in
+
+    def index 
+        @company = Company.alpha 
     end 
 end
--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,27 +1,38 @@
 class RestaurantsController < ApplicationController
+    before_action :set_restaurant, only:[:show, :edit, :update] 
     before_action :redirect_if_not_logged_in
 
-    def index 
-        @restaurants = Restaurant.order_by_rating.includes(:company)  
-    end 
 
     def new 
         @restaurant = Restaurant.new 
-       # @restaurant.companies.build 
+        @restaurant.build_company  
     end 
 
     def create
         @restaurant = Restaurant.new(restaurant_params) 
+        @restaurant.user_id = session[:user_id] 
    
        if @restaurant.save #this is where validations happen
          redirect_to restaurant_path(@restaurant) 
        else
+        @restaurant.build_company 
          render :new
        end
      end
 
+    def index 
+      @restaurants = Restaurant.order_by_rating.includes(:company)  
+    end 
 
     def show 
+    end 
+
+    def update 
+      if @restaurant.update(restaurant_params)
+        redirect_to restaurant_path(@restaurant)
+      else
+        render :edit
+      end 
     end 
 
     private 
