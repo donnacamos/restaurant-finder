27631e52c76113f7156f23e75ceb3a97138696c4
donna <donnacamos88@gmail.com>
2019-10-02 22:58:14 -0400

    display headermethod

--- app/helpers/reviews_helper.rb
+++ app/helpers/reviews_helper.rb
@@ -1,2 +1,10 @@
 module ReviewsHelper
+
+    def display_header(review)
+        if params[:restaurant_id]
+            content_tag(:h1, "Add a Review for #{review.restaurant.name} -  #{review.restaurant.type}")
+        else
+          content_tag(:h1, "Create a review")
+        end
+      end
 end
