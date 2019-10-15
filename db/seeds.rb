eeb00825d6ec06cb54e6040f2c8c5676d75c6817
donna <donnacamos88@gmail.com>
2019-10-15 14:15:58 -0400

    created seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,7 @@
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
 # Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+user1 = User.create(username: "Donna", email: "donna@donna.com", password: "password" ) 
+category1 = Category.create(name: "Mexican") 
+restaurant1 = Restaurant.create(name: "San Joe's Mexican Grill", price_range: "$$", address: "460 S Main St, Dawson, GA 39842", user_id: user1.id, category_id: category1.id)
+review1 = Review.create(stars: "4", title: "Great Food!", content: "Would 100% recommend!", restaurant_id: restaurant1.id, user_id: user1.id) 
